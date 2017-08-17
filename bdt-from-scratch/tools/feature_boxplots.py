aba54508a9f6238eb2d63920959fbf88abfe89dd
ahscodingclub <jake.sauter3@gmail.com>
2017-08-17 09:11:35 -0500

    commenting functionality and producing boxplots for all 64 features

--- bdt-from-scratch/tools/feature_boxplots.py
+++ bdt-from-scratch/tools/feature_boxplots.py
@@ -4,22 +4,15 @@ import numpy as np
 import re
 import sys
 
-"""
-FEATURES
-gabormean_0_2
-IntensityDifference
-MaxIntensity
-SDIntensity
-Compactness
-markov1
-MaxIntensityBG
-SDIntensityBG
-Circularity
-"""
+titles = ['Area', 'ConvexArea', 'Perimeter', 'ConvexPerimeter', 'EquivDiameter', 'MajorAxisLength', 'MinorAxisLength', 'Elongation', 'Compactness', 'Eccentricity', 'Solidity', 'Extent', 'Circularity', 'RadialDistanceSD', 'SecondMoment', 'Roughness', 'MinIntensity', 'MaxIntensity', 'MeanIntensity', 'SDIntensity', 'MinIntensityBG', 'MaxIntensityBG', 'MeanIntensityBG', 'SDIntensityBG', 'IntensityDifference', 'markov1', 'markov2', 'markov3', 'markov4', 'markov5', 'gabormean_0_0', 'gaborSD_0_0', 'gabormean_0_1', 'gaborSD_0_1', 'gabormean_0_2', 'gaborSD_0_2', 'gabormean_1_0', 'gaborSD_1_0', 'gabormean_1_1', 'gaborSD_1_1', 'gabormean_1_2', 'gaborSD_1_2', 'gabormean_2_0', 'gaborSD_2_0', 'gabormean_2_1', 'gaborSD_2_1', 'gabormean_2_2', 'gaborSD_2_2', 'gabormean_3_0', 'gaborSD_3_0', 'gabormean_3_1', 'gaborSD_3_1', 'gabormean_3_2', 'gaborSD_3_2', 'Contrast', 'Correlation', 'Energy', 'Homogeneity', 'Entropy', 'x_3rdordermoment', 'Inversevariance', 'Sumaverage', 'Variance', 'Clustertendency', 'MaxProbability']
 
-atyp_data = [[],[],[],[],[],[],[],[],[]]
-typ_data = [[],[],[],[],[],[],[],[],[]]
+atyp_data = [[]*len(titles)]
+typ_data = [[]*len(titles)]
 
+"""
+Recieving the command line arguments and
+and removing the program name
+"""
 args = sys.argv[1:]
 
 if len(args) == 0:
@@ -31,6 +24,10 @@ elif len(args) == 2:
 else:
     print("Command line arguments should contain: [Definition of typicality(1/2), Percentile of typicality(0-100)]")
 
+"""
+Accessing the nodules IDs, presorted by
+typicality rating
+"""
 nodule_ids = []
 file_name = "atypical_nodules.csv"
 f = open(file_name)
@@ -42,6 +39,10 @@ number_of_nodules = int(percent*len(nodule_ids))
 #setting appropriate atypical nodule ids
 atyp_nodule_ids = []
 
+"""
+Isolating the Xth percentile of nodules from the rest
+and defining them as atypical nodules
+"""
 if defin == 1:
     atyp_nodule_ids = [x[0] for x in nodule_ids[0:number_of_nodules]]
     defin = "distance"
@@ -52,12 +53,15 @@ elif defin == 3:
     atyp_nodule_ids = set([x[0] for x in nodule_ids[0:number_of_nodules]]).intersection(set(x[1] for x in nodule_ids[0:number_of_nodules]))
     defin = "both"
 
-
 print("atyp: ", atyp_nodule_ids)
 f = open("../data/modeBalanced/ModeBalanced_170_LIDC_809_Random.csv", 'r')
 csv_f = csv.reader(f, delimiter=",")
 csv_f.next()
 
+"""
+Acessing the associated features of precaclulated
+typical and atypical nodule ids
+"""
 for row in csv_f:
   atyp = False
   for x in atyp_nodule_ids:
@@ -85,10 +89,6 @@ for row in csv_f:
     typ_data[6].append(float(row[22]))
     typ_data[7].append(float(row[24]))
     typ_data[8].append(float(row[13]))
-
-#NEW NEW NEW
-
-titles = ['Area', 'ConvexArea', 'Perimeter', 'ConvexPerimeter', 'EquivDiameter', 'MajorAxisLength', 'MinorAxisLength', 'Elongation', 'Compactness', 'Eccentricity', 'Solidity', 'Extent', 'Circularity', 'RadialDistanceSD', 'SecondMoment', 'Roughness', 'MinIntensity', 'MaxIntensity', 'MeanIntensity', 'SDIntensity', 'MinIntensityBG', 'MaxIntensityBG', 'MeanIntensityBG', 'SDIntensityBG', 'IntensityDifference', 'markov1', 'markov2', 'markov3', 'markov4', 'markov5', 'gabormean_0_0', 'gaborSD_0_0', 'gabormean_0_1', 'gaborSD_0_1', 'gabormean_0_2', 'gaborSD_0_2', 'gabormean_1_0', 'gaborSD_1_0', 'gabormean_1_1', 'gaborSD_1_1', 'gabormean_1_2', 'gaborSD_1_2', 'gabormean_2_0', 'gaborSD_2_0', 'gabormean_2_1', 'gaborSD_2_1', 'gabormean_2_2', 'gaborSD_2_2', 'gabormean_3_0', 'gaborSD_3_0', 'gabormean_3_1', 'gaborSD_3_1', 'gabormean_3_2', 'gaborSD_3_2', 'Contrast', 'Correlation', 'Energy', 'Homogeneity', 'Entropy', 'x_3rdordermoment', 'Inversevariance', 'Sumaverage', 'Variance', 'Clustertendency', 'MaxProbability']
 
 
 for i in range(0, len(atyp_data)):
